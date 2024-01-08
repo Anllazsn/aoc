932f3bfcc9890e89c23651539585a44a0ab05f32
André Sanson <andrelsanson@gmail.com>
2024-01-07 21:29:21 -0300

    Add readme

--- c#2023/day3/Day3.cs
+++ c#2023/day3/Day3.cs
@@ -9,11 +9,18 @@ namespace c_2023.day3;
 
 public class Day3
 {
-    string[] rawEngineSchematics;
+    public class Engine
+    {
+        public int number;
+        public int pos_start;
+        public int pos_end;
+    }
+    List<Engine> engines;
+
     public Day3()
     {
-        //rawEngineSchematics = File.ReadAllLines("day3/input3.txt");
-        rawEngineSchematics = new string[]
+        //var rawEngineSchematics = File.ReadAllLines("day3/input3.txt");
+        var rawEngineSchematics = new string[]
         {
             "467..114..",
             "...*......",
@@ -26,21 +33,23 @@ public class Day3
             "...$.*....",
             ".664.598.."
         };
+
+        ParseEngineSchematic(rawEngineSchematics);
     }
 
     public void Step1()
     {
         var sum = 0;
-        foreach (string s in rawEngineSchematics)
-        {
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (IsSymbol(s[i]))
-                {
-                    // Save pos
-                }
-            }
-        }
+        //foreach (string s in rawEngineSchematics)
+        //{
+        //    for (int i = 0; i < s.Length; i++)
+        //    {
+        //        if (IsSymbol(s[i]))
+        //        {
+        //            // Save pos
+        //        }
+        //    }
+        //}
         Console.WriteLine($"Day 3 Step 1 {sum}");
     }
 
@@ -50,10 +59,37 @@ public class Day3
         Console.WriteLine($"Day 3 Step 2 {sum}");
     }
 
-    private bool IsSymbol(char c)
+    private void ParseEngineSchematic(string[] raw)
+    {
+        engines = new List<Engine>();
+        var engine = new Engine();
+
+        foreach (string s in raw)
+        {
+            int start_token = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (IsNumber(s[i]))
+                {
+                    start_token = i;
+                }
+                else if (IsSymbol(s[i]))
+                {
+
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+
+    private bool IsNumber(char c)
     {
         switch (c)
         {
+            case '0':
             case '1':
             case '2':
             case '3':
@@ -63,7 +99,16 @@ public class Day3
             case '7':
             case '8':
             case '9':
-            case '0':
+                return true;
+        }
+
+        return false;
+    }
+
+    private bool IsSymbol(char c)
+    {
+        switch (c)
+        {
             case '.':
                 return false;
         }
